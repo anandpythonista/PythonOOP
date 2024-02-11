7845d818bf2cbe8dd5ce49ae9ce7a953421858e7
Anand Thirunavukkarasu <anandt.gct@gmail.com>
2024-02-11 09:25:17 -0500

    OOP example objects

---
+++ OOPChapter1.py
@@ -0,0 +1,24 @@
+class Monster:
+    def __init__(self, health, energy):
+        self.health = health
+        self.energy = energy
+    
+    def update_energy(self, amount):
+        self.energy += amount
+
+    def get_damage(self, amount):
+        self.health -= amount
+        
+class Hero:
+    def __init__(self, damage, monster):
+        self.damage = damage
+        self.monster = monster
+
+    def attack(self):
+        self.monster.get_damage(self.damage)
+
+monster = Monster(100, 100)
+print(monster.health)
+hero = Hero(15, monster)
+hero.attack()
+print(monster.health)    
